f3685d22ce7c1c802b1c612958318e3bf8f8b581
Praveen21-cyber <praveenvelpula2117@gmail.com>
2026-05-12 12:39:45 +0530

    upgrade network sniffer with advance packet analysis

--- sniffer.py
+++ sniffer.py
@@ -1,10 +1,36 @@
-from scapy.all import sniff, IP
+from scapy.all import sniff, IP, TCP, UDP, Raw
 
 def process_packet(packet):
+    print("=" * 60)
+
+    # IP Layer
     if packet.haslayer(IP):
+        print("[IP Layer]")
         print("Source IP:", packet[IP].src)
         print("Destination IP:", packet[IP].dst)
         print("Protocol:", packet[IP].proto)
-        print("-" * 50)
 
-sniff(count=10, prn=process_packet)
+    # TCP Layer
+    if packet.haslayer(TCP):
+        print("[TCP Layer]")
+        print("Source Port:", packet[TCP].sport)
+        print("Destination Port:", packet[TCP].dport)
+
+    # UDP Layer
+    elif packet.haslayer(UDP):
+        print("[UDP Layer]")
+        print("Source Port:", packet[UDP].sport)
+        print("Destination Port:", packet[UDP].dport)
+
+    # Payload (Data inside packet)
+    if packet.haslayer(Raw):
+        print("[Payload]")
+        print(packet[Raw].load)
+
+    # Packet Length
+    print("Packet Length:", len(packet))
+
+    print("=" * 60)
+
+# Capture 15 packets
+sniff(count=15, prn=process_packet)
